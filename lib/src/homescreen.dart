4fc4eac868251e70f809f601debaa9b2f8e44d57
davidguzman1693 <dmgd93@gmail.com>
2020-04-13 20:23:14 +0200

    Stable version from second step

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatefulWidget{
 class _HomeScreenState extends State<HomeScreen>{
   
   @override
-  Widget build(BuildContext){
+  Widget build(BuildContext context){
     return Scaffold(
         body: ListView(
             padding: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
