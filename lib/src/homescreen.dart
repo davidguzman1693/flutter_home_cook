22897c4f2b5c091a5cb322c54c6f9a57ce335b05
davidguzman1693 <dmgd93@gmail.com>
2020-04-27 19:21:39 +0200

    Vid 3,4,5. Adding a columng for most bought items and search field

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:home_cook/src/widgets/food_category.dart';
 import 'widgets/home_top_info.dart';
+import 'widgets/search_field.dart';
+import 'widgets/bought_foods.dart';
+
+//Data
+import 'data/food_data.dart';
 
 class HomeScreen extends StatefulWidget{
     @override
@@ -9,6 +14,8 @@ class HomeScreen extends StatefulWidget{
 
 class _HomeScreenState extends State<HomeScreen>{
   
+  List<Food> _foods = foods;
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -17,8 +24,53 @@ class _HomeScreenState extends State<HomeScreen>{
             children: <Widget>[
               HomeTopInfo(),
               FoodCategory(),
+              SizedBox(height: 20.0,),
+              SearchField(),
+              SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Frequently bought Foods",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  GestureDetector(
+                      onTap: (){},
+                      child: Text(
+                      "View All",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orangeAccent
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0,),
+              Column(
+                children: _foods.map(_buildFoodItems).toList(),
+              ),
             ],
         ),
     );
   }
+
+  Widget _buildFoodItems(Food food){
+    return Container(
+      margin: EdgeInsets.only(bottom: 20.0),
+      child: BoughtFoods(
+        id: food.id,
+        name: food.name,
+        imagePath: food.imagePath,
+        category: food.category,
+        discount: food.discount,
+        price: food.price,
+        ratings: food.ratings,
+      ),
+    );
+  }
+
 }
